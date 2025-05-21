ab84a5f35ef0baccebb810b3c8967cfe5f727418
BeiyaC <anis.grigah@le-campus-numerique.fr>
2025-05-21 13:37:49 +0200

    add a product card component

--- components/productCard.vue
+++ components/productCard.vue
@@ -1,3 +1,38 @@
 <template>
-
+  <UCard class="cursor-pointer hover:scale-105 transition-transform">
+    <img
+        :src="`https://picsum.photos/500`"
+        :alt="product.name"
+        class="rounded-lg mb-2 w-full h-56 object-cover bg-gray-200"
+    />
+    <div>
+      <h2 class="font-semibold text-lg truncate">{{ product.name }}</h2>
+      <div class="text-xs text-gray-400 mb-1">
+        {{ product.description }}
+      </div>
+      <div class="text-xs text-gray-400 mb-1">
+        {{ product.price }}
+      </div>
+      <div class="mt-2 text-sm">
+          <span class="font-bold text-primary">
+            ★ {{ product.stock ?? '--' }}/10
+          </span>
+      </div>
+    </div>
+  </UCard>
 </template>
+
+<script setup lang="ts">
+interface Product {
+  id: string
+  name: string
+  description: string
+  price: number
+  stock: number
+}
+
+defineProps<{
+  product: Product
+}>()
+
+</script>
