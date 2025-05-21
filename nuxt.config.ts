3e52670c6adbc874574bca25aa5c09973d01171e
BeiyaC <anis.grigah@le-campus-numerique.fr>
2025-05-21 13:19:05 +0200

    init project

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+    },
+  },
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts'
+  ],
+  css: ['~/assets/css/main.css']
+})
